5e261d357dc30963d8d72d0441e60c7edd2c6904
Ahmed Jaafar <aajaafar@umich.edu>
2022-11-12 12:02:41 -0500

    freeze

--- index/index.py
+++ index/index.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
+    
 @app.route("/")
 def index():
     return render_template("index.html")
--- resume/resume.py
+++ resume/resume.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
+    
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
